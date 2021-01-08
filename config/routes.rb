dbf573f4aac02fba3ad3b282a8e1a29b80128bea
Wanjin Yoo <dksladjsla@gmail.com>
2021-01-08 05:26:52 +0000

    implement login system

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,24 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+
+# User authentication
+    
+    # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+ 
+
+# Basic routes
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resource :about, only: [:show]
+
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -14,8 +30,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products,:categories, except: [:edit, :update, :show]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
